41d91b19dec251dd9e46ec947f69f2d3f580d5bc
Matt Barlow <83474131+mattbarlow-sg@users.noreply.github.com>
2023-08-09 10:03:47 -0400

    create athena alb module (#1)
    * create athena alb module

--- variables.tf
+++ variables.tf
@@ -1,13 +1,41 @@
-variable "name" {
-  description = "name to prepend to all resource names within module"
+# variable "input_tags" {
+#   description = "Map of tags to apply to resources"
+#   type        = map(string)
+#   default = {
+#     Developer   = "StratusGrid"
+#     Provisioner = "Terraform"
+#   }
+# }
+
+variable "alb_name" {
+  description = "Name of the Application Load Balancer"
   type        = string
 }
 
-variable "input_tags" {
-  description = "Map of tags to apply to resources"
-  type        = map(string)
-  default = {
-    Developer   = "StratusGrid"
-    Provisioner = "Terraform"
-  }
+variable "workgroup_name" {
+  description = "Name of the workgroup to use for the query and query results"
+  type        = string
+}
+
+variable "database_name" {
+  description = "Name of the database to use for the table creation"
+  type        = string
+}
+
+variable "partition_table" {
+  description = "Whether or not to create table partitioning. Recommended for large ALB sets"
+  type        = bool
+  default     = true
+}
+
+variable "query_name" {
+  description = "Optionally provide a name for the saved query that creates the table"
+  type        = string
+  default     = ""
+}
+
+variable "table_name" {
+  description = "Optionally provide a name for the table"
+  type        = string
+  default     = ""
 }
